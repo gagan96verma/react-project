550328a3e884a1efee5203153249f8bc75b8b3d2
gaganverma <gagan.verma@engineer.ai>
2020-06-10 15:39:11 +0530

    Updates

---
+++ precache-manifest.a658b06068c5c7e2cb5f5bcdf13591f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f7fd65c8360c01fce971f3f1124ec7",
+    "url": "/gagan96verma/react-project/index.html"
+  },
+  {
+    "revision": "01e444c3828f805b994b",
+    "url": "/gagan96verma/react-project/static/css/main.4b847592.chunk.css"
+  },
+  {
+    "revision": "a01143d854b206e372ca",
+    "url": "/gagan96verma/react-project/static/js/2.b9109df8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gagan96verma/react-project/static/js/2.b9109df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e444c3828f805b994b",
+    "url": "/gagan96verma/react-project/static/js/main.cbf22770.chunk.js"
+  },
+  {
+    "revision": "980d11628bb2104b5979",
+    "url": "/gagan96verma/react-project/static/js/runtime-main.a63aee25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gagan96verma/react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
